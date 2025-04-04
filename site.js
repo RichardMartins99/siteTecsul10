e08bb1b9162b91d5e9344cb099eacdd8beee1d3f
Richard Martins <77687618+RichardMartins99@users.noreply.github.com>
2025-04-03 23:04:29 -0300

    adicionado botao de saiba mais em card e adicionado animação em carousel

--- site.js
+++ site.js
@@ -1,9 +1,13 @@
+let isModalOpen = false; 
 document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById("meuModal");
     const spanFechar = document.getElementsByClassName("fechar")[0];
+    const reels = document.querySelectorAll(".carousel .reel");
 
     function fecharModal() {
+        isModalOpen = false;
         modal.style.display = "none";
+        reels.forEach(reel => reel.style.animationPlayState = "running");
     }
 
     spanFechar.onclick = fecharModal;
@@ -19,11 +23,44 @@ document.addEventListener('DOMContentLoaded', function() {
             fecharModal();
         }
     }
+
+    reels.forEach(reel => {
+        reel.addEventListener("mouseenter", () => {
+            if (!isModalOpen) {
+                reel.style.animationPlayState = "paused";
+            }
+        });
+    
+        reel.addEventListener("mouseleave", () => {
+            if (!isModalOpen) {
+                reel.style.animationPlayState = "running";
+            }
+        });
+    });
+    
+    document.querySelectorAll(".carousel").forEach(ajustarDuracao);
 });
 
+function ajustarDuracao(carousel) {
+    const reel = carousel.querySelector(".reel");
+    const larguraReel = reel.scrollWidth; // Pega a largura total do reel
+    const duracaoBase = 2; // Tempo base para um carrossel de referência
+    const larguraPadrao = 400; // Tamanho base do primeiro carrossel
+
+    // Ajusta a duração proporcionalmente ao tamanho
+    const novaDuracao = (larguraReel / larguraPadrao) * duracaoBase;
+
+    // Aplica a nova duração na animação
+    reel.style.animationDuration = `${novaDuracao}s`;
+}
+
 function abrirModal(idModal){
+    isModalOpen = true;
     const modal = document.getElementById("meuModal");
+    const reels = document.querySelectorAll(".carousel .reel");
     modal.style.display = "block";
+
+    reels.forEach(reel => reel.style.animationPlayState = "paused");
 
     configModal(idModal);
 }
